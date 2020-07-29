eba04a26a5a1bad74ffbf1da72cc7691f27376f0
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-07-28 21:32:42 -0300

    exemplo com CONVERT

--- Colinha/CONVERTER DADOS.sql
+++ Colinha/CONVERTER DADOS.sql
@@ -3,5 +3,8 @@
 SELECT CAST("2017-08-29" AS DATE);
 SELECT CONVERT("2017-08-29", DATE);
 
-/* Converte e Limita em 5char */
+/* Converte e Limita em 5 char */
 SELECT CAST(ClienteNome AS CHAR(5)) FROM clientes;
+
+/* Converte e Limita em 5 char */
+SELECT CONVERT(ClienteNome, CHAR(5)) FROM clientes;
