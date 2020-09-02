a8c84dd76f69305255492cfeaf7fc9a431090045
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-09-01 21:33:33 -0300

    adição de outro exemplo

--- Colinha/procedure.sql
+++ Colinha/procedure.sql
@@ -17,3 +17,23 @@ INNER JOIN Contas on Clientes.ClienteCodigo  = Contas.ClienteCodigo
 WHERE Clientes.ClienteCodigo = @CodigoCliente;
 
 EXEC uspRetornaIdade 1
+
+-- Com dois Parametros e variáveis
+
+CREATE PROCEDURE uspRetornaOpcao
+@opcao int,
+@valor float
+
+AS
+
+IF @opcao = 1
+	BEGIN
+		SELECT 'Você escolheu a opção', @opcao, @valor
+	END
+ELSE
+	BEGIN
+		DECLARE @opcao_interna int = 99, @valor_interna int = 99
+		SELECT 'Você escolheu a opção', @opcao_interna, @valor_interna
+	END
+
+EXEC uspRetornaOpcao 1, 2.0
