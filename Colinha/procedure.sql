149f041f17b9ee5a07c07af5500f4313e792ff46
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-09-01 21:17:33 -0300

    adição de procedure com parametro

--- Colinha/procedure.sql
+++ Colinha/procedure.sql
@@ -5,3 +5,15 @@ FROM Clientes
 INNER JOIN Contas on Clientes.ClienteCodigo  = Contas.ClienteCodigo
 
 EXEC uspRetornaIdadeSimples
+
+-- Com Parametro
+
+CREATE PROCEDURE uspRetornaIdade
+@CodigoCliente int
+AS
+SELECT Clientes.ClienteNome, YEAR (GETDATE())-YEAR (ClienteNascimento) AS IDADE
+FROM Clientes
+INNER JOIN Contas on Clientes.ClienteCodigo  = Contas.ClienteCodigo
+WHERE Clientes.ClienteCodigo = @CodigoCliente;
+
+EXEC uspRetornaIdade 1
